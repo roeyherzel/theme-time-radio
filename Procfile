a96013d833a1b51064999c9e54139dcc2951b7ab
Roy Herzel <royherzel@gmail.com>
2016-09-28 13:32:20 -0700

    Heroku - change config.py and add seed to init task

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn archive:app
+init: python manage.py seed
--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
-DEBUG = True
-# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flasker'
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SERVER_NAME = "localhost:5000"
-PREFERRED_URL_SCHEME = "http"
+if os.environ.get('DATABASE_URL') is not None:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+else:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flasker'
+
+# SQLALCHEMY_TRACK_MODIFICATIONS = True
+# SERVER_NAME = "localhost:5000"
+# PREFERRED_URL_SCHEME = "http"
