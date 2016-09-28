000f907637476fe61c660fd86080029a6f979f1c
Roy Herzel <royherzel@gmail.com>
2016-09-28 13:07:43 -0700

    Heroku - change SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flasker'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flasker'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SERVER_NAME = "localhost:5000"
 PREFERRED_URL_SCHEME = "http"
