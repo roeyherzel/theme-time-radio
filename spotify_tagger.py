22e6443ef353d7413edd1269838a0965ead95e6a
Roy Herzel <royherzel@gmail.com>
2016-10-19 12:33:44 -0700

    implement TracksSpotifyData relation

--- spotify_tagger.py
+++ spotify_tagger.py
@@ -21,28 +21,33 @@ def search(query):
     return res[query_type + 's']['items']
 
 
-def searchTrack(song, artist):
+def searchTrack(song, artist, track_id):
     # search track
     results = search('track:{} artist:{}'.format(song, artist))
     if len(results) > 0:
-        collectSpotifyData(results[0], 'track')
+        collectSpotifyData(results[0], 'track', track_id)
     else:
         # search artist
         results = search('artist:{}'.format(artist))
         if len(results) > 0:
-            collectSpotifyData(results[0], 'artist')
+            collectSpotifyData(results[0], 'artist', track_id)
         else:
             print("Error...didn't find track for: {} / {}".format(song, artist))
 
 
-def collectSpotifyData(item, match_type):
+def collectSpotifyData(data, match_type, track_id):
     if match_type == 'track':
         # TODO: handle multiple artists
-        CollectArtistData(item['artists'][0])
-        CollectAlbumData(item['album'])
-        CollectSongData(item)
+        myTracksData = models.TracksSpotifyData(
+            track_id=track_id,
+            artist_id=CollectArtistData(data['artists'][0]).id,
+            album_id=CollectAlbumData(data['album']).id,
+            song_id=CollectSongData(data).id
+        )
     else:
-        CollectArtistData(item)
+        myTracksData = models.TracksSpotifyData(track_id=track_id, artist_id=CollectArtistData(data).id)
+
+    models.TracksSpotifyData.create(myTracksData)
 
 
 class BaseResource(object):
@@ -51,7 +56,6 @@ class BaseResource(object):
         self.name = data['name']
         self.url = data['href']
         self.type = data['type']
-
         self.myModel = self.Model(spotify_id=self.id, name=self.name, endpoint_url=self.url)
         print(self.myModel)
 
@@ -63,6 +67,9 @@ class BaseResource(object):
         models.Images.create(models.Images(**image))
         self.myModel.image = image['url']
 
+    def create(self):
+        self.Model.create(self.myModel)
+
 
 class CollectArtistData(BaseResource):
     def __init__(self, data):
@@ -72,7 +79,10 @@ class CollectArtistData(BaseResource):
             data = spotify.artist(self.id)
 
         self.addImage(data['images'])
-        self.Model.create(self.myModel)
+        self.create()
+
+    def __repr__(self):
+        return self.id
 
 
 class CollectAlbumData(BaseResource):
@@ -80,19 +90,18 @@ class CollectAlbumData(BaseResource):
         self.Model = models.SpotifyAlbums
         super().__init__(data)
         self.addImage(data['images'])
-        self.Model.create(self.myModel)
+        self.create()
 
 
 class CollectSongData(BaseResource):
     def __init__(self, data):
         self.Model = models.SpotifySongs
-        self.external_ids = data.get('external_ids', None)  # only relevant for song
         super().__init__(data)
         self.myModel.preview_url = data['preview_url']
         self.addImage(data['album']['images'])
-        self.Model.create(self.myModel)
+        self.create()
 
 
 with app.app_context():
     for myTrack in models.Tracks.query.filter_by(resolved=True).limit(10):
-        searchTrack(song=myTrack.song, artist=myTrack.artist)
+        searchTrack(song=myTrack.song, artist=myTrack.artist, track_id=myTrack.id)
