b0e202d79c71b710e6b015992ac7c9345ff37152
Roy Herzel <royherzel@gmail.com>
2016-11-04 14:47:45 -0700

    change all artist pagination to A-Z indexing

--- archive/views.py
+++ archive/views.py
@@ -1,8 +1,11 @@
 from flask import jsonify, render_template, request, send_file
 
 from archive import app
+from archive.models import db
 from archive.models.podcast import Episodes
 from archive.models.spotify import Artists
+
+from sqlalchemy import func
 
 import re
 from jinja2 import evalcontextfilter, Markup, escape
@@ -28,17 +31,31 @@ def mixtapes_view(tape_name=None):
 
 
 @app.route('/artists')
-@app.route('/artists/page/<int:page>')
 @app.route('/artists/<string:artist_id>')
-def artist_view(page=1, artist_id=None):
+@app.route('/artists/index/<string:index>')
+def artist_view(index="A", artist_id=None):
     if artist_id is not None:
         res = Artists.query.get(artist_id)
         if res is None:
             res = Artists.query.filter(Artists.lastfm_name == artist_id).one()
         return render_template('artist.html.jinja', artist=res)
 
-    artists = Artists.query.order_by(Artists.name).paginate(page, 50, False)
-    return render_template('all_artists.html.jinja', artists=artists)
+    index = index.upper()
+    artists_names = db.session.query(Artists.name).order_by(Artists.name).all()
+
+    # build uniqe list of sorted artist names index
+    artists_index = [re.sub(r"[^a-zA-Z]", "0", i[0][0].upper()) for i in artists_names]
+    artists_index = sorted(set(artists_index))
+
+    # get artist objects matching index
+    if index == "0":
+        # didn't find a method to performe the filtering in db query
+        artists = [a for a in Artists.query.all() if re.match(r"[^a-zA-Z]", a.name[0])]
+    else:
+        artists = Artists.query.order_by(Artists.name).filter(func.upper(Artists.name).startswith(index)).all()
+
+    print(artists)
+    return render_template('all_artists.html.jinja', artists=artists, index_list=artists_index)
 
 
 @app.route('/episodes')
