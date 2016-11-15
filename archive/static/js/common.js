26489ff25ece7175e272ce62b885a268382e0bd9
Roy Herzel <royherzel@gmail.com>
2016-11-15 15:05:04 -0800

    add google anayltics

--- archive/static/js/common.js
+++ archive/static/js/common.js
@@ -4,17 +4,11 @@ String.prototype.nl2br = function() {
 }
 
 function getTemplateAjax(path, callback) {
-  var source;
-  var template;
-
   $.ajax({
-    url: $SCRIPT_ROOT + 'static/js/templates/' + path,
+    url: `${$SCRIPT_ROOT}static/js/templates/${path}`,
     cache: false,
     success: function(data) {
-      source = data
-      template  = Handlebars.compile(source);
-
-      if (callback) callback(template);
+      if (callback) callback(Handlebars.compile(data));
     }
   });
 }
