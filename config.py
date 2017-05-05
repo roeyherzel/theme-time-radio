7a87e8cbc4655b587d647304899b47a9aa60c2cb
Roey Herzel <royeherzel@gmail.com>
2017-05-05 11:04:31 -0700

    remove DEBUG mode from hosted environment

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 import os
 
-if os.environ.get('DATABASE_URL') is not None:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-else:
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/flasker'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///themetime.sqlite3'
+if os.environ.get('HOSTED') is not True:
     DEBUG = True
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///themetime.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-# SERVER_NAME = "localhost:5000"
-# PREFERRED_URL_SCHEME = "http"
